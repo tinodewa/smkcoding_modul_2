54b5d15a9725ef24011fe2bc0d09cbb8ba5a7ba7
tinodewa <dtino0812@gmail.com>
2020-05-12 12:35:00 +0700

    Complete Fragment and Viewpager modul

--- app/src/main/java/com/smkcoding/smkcoding2/MainActivity.kt
+++ app/src/main/java/com/smkcoding/smkcoding2/MainActivity.kt
@@ -2,11 +2,29 @@ package com.smkcoding.smkcoding2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
+import com.google.android.material.tabs.TabLayoutMediator
+import com.google.android.material.tabs.TabLayoutMediator.TabConfigurationStrategy
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val menuTeks = arrayOf("Teman", "Github", "Profil")
+    val menuIcon = arrayOf(R.drawable.ic_chat, R.drawable.ic_github, R.drawable.ic_user)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = ViewPagerAdapter(this)
+        view_pager.setAdapter(adapter);
+        TabLayoutMediator(tab_layout, view_pager,
+            TabConfigurationStrategy { tab, position ->
+                tab.text = menuTeks[position]
+                tab.icon = ResourcesCompat.getDrawable(
+                    resources,
+                    menuIcon[position], null
+                )
+            }).attach()
     }
 }
