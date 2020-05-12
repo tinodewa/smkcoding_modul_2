d95f30a6061f94de4bd066cf1a4cf5e431015cd3
tinodewa <dtino0812@gmail.com>
2020-05-12 14:30:38 +0700

    Complete RecyclerView modul

--- app/src/main/java/com/smkcoding/smkcoding2/MyFriendsFragment.kt
+++ app/src/main/java/com/smkcoding/smkcoding2/MyFriendsFragment.kt
@@ -6,8 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.fragment_my_friends.*
 
 class MyFriendsFragment : Fragment() {
+
+    lateinit var listTeman : ArrayList<MyFriend>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +28,27 @@ class MyFriendsFragment : Fragment() {
 
     override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun simulasiDataTeman() {
+        listTeman = ArrayList()
+        listTeman.add(MyFriend("Fakhry","Laki-Laki","fakhry@smkcoding.id","081123123123","Malang"))
+        listTeman.add(MyFriend("Ahmad","Laki-Laki","Ahmad@smkcoding.id","085123123123","Malang"))
+    }
+
+    private fun tampilTeman() {
+        rv_listMyFriends.layoutManager = LinearLayoutManager(activity)
+        rv_listMyFriends.adapter = MyFriendAdapter(activity!!, listTeman)
+    }
+
+    private fun initView() {
+        simulasiDataTeman()
+        tampilTeman()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        this.clearFindViewByIdCache()
     }
 }
